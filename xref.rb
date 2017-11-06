08da0f615eb46af6ae7dcd3d2cef1a1d23443f6f
spirography <jeremyspiro@gmail.com>
2017-11-05 19:55:54 -0500

    deleted old code, implemented new html writing method

--- xref.rb
+++ xref.rb
@@ -49,6 +49,8 @@ sources = Hash.new()
 
 dh = Hash.new()
 uri = nil
+prev_line = -1 # last source code line looked at, needed to merge some instructions with their sequential lines
+prev_addr = 0 # same purpose as prev_line
 # store the dwarfdump information in a more useful way, and add extra info to the entries
 dwarfarray.each { |x|
     # parse address from assembly
@@ -73,58 +75,26 @@ dwarfarray.each { |x|
     # 4. uri
     entry_line = x[1].to_i(10)
     # TODO: combine ET sections with the previous one?  we probably want only one dwarfdump entry for a given assembly instruction, not several
-    entry = 0
-    entry = [addr, (entry_line > sources[uri] ? sources[uri]+1 : entry_line), entry_line, (entry_line < sources[uri]), uri]
-    if (entry_line > sources[uri])
-        sources[uri] = entry_line
+    if (entry_line != prev_line)
+        entry = 0
+        entry = [addr, (entry_line > sources[uri] ? sources[uri]+1 : entry_line), entry_line, (entry_line < sources[uri]), uri]
+        if (entry_line > sources[uri])
+            sources[uri] = entry_line
+        end
+        if (dh[addr] == nil)
+            dh[addr] = entry
+        elsif # we already have souce code for this address
+            dh[addr][2] = entry_line
+        end
+    else # make it a continuation of the previously parsed dd instruction
+        x = 0 # do nothing
+                
     end
-    if (dh[addr] == nil)
-        dh[addr] = [entry]
-    elsif # we already have souce code for this address
-        dh[addr].push(entry)
-    end
+    
+    prev_line = entry_line
+    prev_addr = addr
 }
 puts dh
-
-
-# TODO: open up source files, mark when lines are visited
-
-# global variables used over several iterations
-cur_file = '' # the source file of the last asm line successfully matched in dwarfdump
-cur_line = '' # the sc line of the last asm line successfully matched in dwarfdump
-
-html_table = '' # the contents of the <table> tag
-html_asm = '' # formatted HTML of the current cell for the assembly side
-html_source = '' # formatted c code of the current cell for the source code side
-
-
-# iterate over objdump assembly to build the webpage
-asmarray.each { |x|
-    # look up line in dwarfdump
-    correspondance = dh[x[0].to_i(16)]
-    if (correspondance != nil) # if we find a match, we probably create a new table row
-        puts x[0]
-        if (correspondance[2] != cur_file)
-        # check if the current file changed (load that file's code if we haven't already)
-        # check if we need to create a new row
-            # if we do, append the finished row and reset the html_* vars
-            # if we do, get all the source code for that row immediately
-            x = 0
-        end
-    else # append the assembly to the html_asm side
-        puts 'foo'
-    end
-}
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -139,162 +109,89 @@ def get_file_array(path)
     return array
 end
 
-
-header = File.open("header.txt").read
-
-footer = "</body></html>"
-
-body = '<table class="dump">'
-
-#asmarray.each { |x|
-#    puts x[0] + ", " + x[1] + ", " + x[2] + ", " + x[3] + "\t\t(" + (dh[x[0].to_i(16)] != nil ? dh[x[0].to_i(16)] : "Nil") + ")"
-#}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-=begin
-# iterate over all the lines we got from the dwarfdump output
-# that way, we build the information for one file at a time
-enum = asmarray.each
-cur_enum = enum.next
-
-content = ""
-side_source = ""
-side_asm = ""
-
-
-source = []
-cur_addr = 0
-cur_line = 0
-prev_addr = 0
-prev_line = 0
-prev_line2 = 0
-max_line = 0
-max_line2 = 0
-dwarfarray.each { |x|
-    cur_addr = x[0].to_i(16)
-    cur_line = x[1].to_i(10)
-    if (x[4] != nil)
-        puts "uri: " + x[4]
-        source = get_file_array(x[4])
-        side_source = ""
-        side_asm = ""
-    end
-    if (prev_line > 0)
-        #printf("current: 0x%x (%d) (%d)\n", prev_addr, prev_line, max_line)
-    end
-                
-                
-    if (prev_line != prev_line2)
-        content += '<tr><td>' + side_source + '</td><td>' + side_asm + '</td>'
-        side_source = ""
-        side_asm = ""
-    end
-    # print source
-    # rules: if prev_line < max_line, print just prev_line
-    #        if prev_line == prev_line2, don't print
-    #        else print from old max to prev_line-1
-    #printf("addr: 0x%x\t", prev_addr)
-    
-    if prev_line == prev_line2
-        
-    elsif prev_line < max_line
-        line = (source[prev_line-1] == "" ? '&nbsp;' : source[prev_line-1].strip)
-        line.sub! '<', '&lt;'
-        line.sub! '>', '&gt;'
-        side_source += '<div class="src-line"><div>' + line + '</div></div>'
-        puts source[prev_line-1]
+def htmlify_string(s)
+    s = s.rstrip
+    if (s == '')
+        return '&nbsp;'
     else
-        (max_line2..prev_line-1).each { |i|
-            line = (source[i] == "" || source[i] == nil ? '&nbsp;' : source[i].strip)
-            line.sub! '<', '&lt;'
-            line.sub! '>', '&gt;'
-            side_source += '<div class="src-line"><div>' + line + '</div></div>'
-            puts source[i]
-        }
+        return s.gsub('\n', '') #remove trailing newlines
+        .gsub('<', '&lt;')
+
     end
-                
-    # add all assembly in the address range to the current chunk
-    while cur_addr > cur_enum[0].to_i(16) do
-        puts cur_enum[0] + ", " + cur_enum[1] + ", " + cur_enum[2] + ", " + cur_enum[3]
-        side_asm += '<div class="asm-line"><div>' + cur_enum[0] + '</div><div>' + cur_enum[1] + '</div><div>' + cur_enum[2] + '</div><div>' + (cur_enum[3] == "" ? "&nbsp;" : cur_enum[3]) + '</div></div>'
-        cur_enum = enum.next
-    end
-    prev_line2 = prev_line
-    prev_addr = cur_addr
-    prev_line = cur_line
-    max_line = (prev_line > max_line ? prev_line : max_line)
-    max_line2 = (prev_line2 > max_line2 ? prev_line2 : max_line2)
-    # if cur_line is different from prev_line, we found the end of a "chunk": wrap it up as a table row, paste in the matching source code lines, and start the next chunk
-}
-# note that last line in dwarf contains the final address, so we iterate over the enum differently
-#printf("current: 0x%x (%d)\n", prev_addr, prev_line)
-puts cur_enum[0] + ", " + cur_enum[1] + ", " + cur_enum[2] + ", " + cur_enum[3]
-while cur_addr > cur_enum[0].to_i(16) do
-    cur_enum = enum.next
-    side_asm += '<div class="asm-line"><div>' + cur_enum[0] + '</div><div>' + cur_enum[1] + '</div><div>' + cur_enum[2] + '</div><div>' + (cur_enum[3] == "" ? "&nbsp;" : cur_enum[3]) + '</div></div>'
-    puts cur_enum[0] + ", " + cur_enum[1] + ", " + cur_enum[2] + ", " + cur_enum[3]
 end
-side_asm += '<div class="asm-line"><div>' + cur_enum[0] + '</div><div>' + cur_enum[1] + '</div><div>' + cur_enum[2] + '</div><div>' + (cur_enum[3] == "" ? "&nbsp;" : cur_enum[3]) + '</div></div>'
 
 
-content += '<tr><td>' + side_source + '</td><td>' + side_asm + '</td>'
-side_source = ""
-side_asm = ""
 
+# TODO: open up source files, mark when lines are visited
 
-File.write("index.html", header + '<table class="dump">' + content + '</table>' + footer)
+# global variables used over several iterations
+cur_file = '' # the source file of the last asm line successfully matched in dwarfdump
+cur_line = '' # the sc line of the last asm line successfully matched in dwarfdump
 
-code_side = ""
-asm_side = ""
+html_table = '' # the contents of the <table> tag
+html_asm = '' # formatted HTML of the current cell for the assembly side
+html_source = '' # formatted c code of the current cell for the source code side
+
+first_iteration = true
+
+# iterate over objdump assembly to build the webpage
 asmarray.each { |x|
-    if (dh[x[0].to_i(16)] != nil)
-        if (true)
-            body += "<tr><td>" + code_side + "</td><td>" + asm_side + "</td></tr>"
+    # NOTE: do not want to iterate over every line
+    # helper frames should be ignored
+    # look up line in dwarfdump
+    correspondance = dh[x[0].to_i(16)]
+    if (correspondance != nil) # if we find a match, we probably create a new table row
+        # cut off the old table row
+        if (first_iteration)
+            first_iteration = false
+        else
+            html_table += '<tr><td>' + html_source + '</td><td>' + html_asm + '</td></tr>'
+            html_asm = '';
+            html_source = '';
+            first_iteration = false
         end
-        code_side = ""
-        asm_side = ""
+        if (correspondance[4] != cur_file)
+            cur_file = correspondance[4]
+            sources[cur_file] = get_file_array(cur_file)
+        end
+        puts correspondance
+        # get all the corresponding source code
+        sources[correspondance[4]][correspondance[1]-1..correspondance[2]-1].each_with_index do |line, index|
+            html_source += '<div class="src-line"><div>' + (index+correspondance[1]).to_s + '.</div><div>' + htmlify_string(line) + '</div></div>'
+        end
+        
+        #if (correspondance[2] != cur_file)
+            
+            
+        # check if the current file changed (load that file's code if we haven't already)
+        # check if we need to create a new row
+            # if we do, append the finished row and reset the html_* vars
+            # if we do, get all the source code for that row immediately
+        #end
     end
-    # add in the div containing the line of assembly we are currently looking at
-    asm_side += '<div class="asm-line"><div>' + x[0] + '</div><div>' + x[1] + '</div><div>' + x[2] + '</div><div>' + (x[3] == "" ? "&nbsp;" : x[3]) + '</div></div>'
+    # add the current line of assembly to the row
+    html_asm += '<div class="asm-line"><div>' + x[0] + '</div><div>' + x[1] + '</div><div>' + x[2] + '</div><div>' + (x[3] == "" ? "&nbsp;" : x[3]) + '</div></div>'
 }
-body += "<tr><td>" + code_side + "</td><td>" + asm_side + "</td></tr>"
 
-body += "
-</table>"
+# close off current row
+html_table += '<tr><td>' + html_source + '</td><td>' + html_asm + '</td></tr>'
 
-#File.write("index.html", header + body + footer)
-=end
+# write to file
+header = File.open("header.txt").read
+footer = '</body></html>'
+
+File.write("index.html", header + '<table class="dump">' + html_table + '</table>' + footer)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
